110c49336f53fd10216bcc6e04c7f61047e16c6e
101 <101@ctt.perm.ru>
2019-10-12 18:02:15 +0500

    hello

---
+++ ConsoleApplication1/ConsoleApplication1/Presentor.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    public class Comporator : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            if(x < y)
+            {
+                return 1;
+            }
+            else if(x > y)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+    }
+    public class Presentor
+    {
+        Random random = new Random();
+        private int[] array;
+        private int arrayLength;
+        public Presentor()
+        {
+            arrayLength = 10;
+            array = new int[arrayLength];
+            //заполнение массива
+            for (int i = 0; i < 10; i++) { array[i] = random.Next(0, 1000); }
+        }
+
+        public String Add(String digitStr)
+        {
+            String message = "";
+            int digit = new int();
+            if (int.TryParse(digitStr, out digit)) {
+                if (digit >= 0)
+                {
+                    arrayLength += 1;
+                    int[] tmpArray = new int[arrayLength];
+                    array.CopyTo(tmpArray, 0);
+                    tmpArray[arrayLength - 1] = digit;
+                    array = tmpArray;
+                    message = "Значение " + digit + " добавлено в массив";
+                }
+                else
+                {
+                    message = "Значение должно быть положительным числом числом";
+                }
+            } else {
+                message = "Значение не является числом";
+            }
+            return message; 
+        }
+
+        public String Delete(String indexStr)
+        {
+            //удаление по индексу
+            String message = "";
+            int index = new int();
+            if (int.TryParse(indexStr, out index))
+            {
+                if ((index < arrayLength) && (index >= 0 ))
+                {
+                    arrayLength -= 1;
+                    int[] tmpArray = new int[arrayLength];
+                    for(int i = 0; i < index; i++)
+                    {
+                        tmpArray[i] = array[i];
+                    }
+                    for (int i = index; i < tmpArray.Length; i++)
+                    {
+                        tmpArray[i] = array[i + 1];
+                    }
+                    array = tmpArray;
+                    message = "Значение по индексу " + index + " удалено из массива";
+                }
+                else
+                {
+                    message = "Введенное число не является индексом тиекущего массива";
+                }
+            }
+            else
+            {
+                message = "Индекс не является числом";
+            }
+            return message;
+        }
+        
+        public String GetArrayString()
+        {
+            String arrayStr = "";
+            foreach (int numb in array) arrayStr += numb + " ";
+            return arrayStr;
+        }
+
+        public void SortArray()
+        {
+            List<int> numbsToSort = new List<int>();
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (((array[i] % 2) == 0) && array[i] != 0)
+                {
+                    numbsToSort.Add(array[i]);
+                    array[i] = -1;
+                }
+            }
+            Comporator comporator = new Comporator();
+            numbsToSort.Sort(comporator);
+            int currentListIndex = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == -1)
+                {
+                    array[i] = numbsToSort.ElementAt(currentListIndex);
+                    currentListIndex++;
+                }
+            }
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1 {
+    class Program {
+        static Presentor presentor;
+        static void Main(string[] args) {
+            bool exit = false;
+            presentor = new Presentor();
+            while (!exit) {
+                Console.WriteLine("Введите данные\n" +
+                                "1 - Добавить элемент\n" +
+                                "2 - Удалить элемент\n" +
+                                "3 - Вывести значения массива\n" +
+                                "4 - Сортировка\n" +
+                                "0 - Выйти");
+                String userAnswerStr = Console.ReadLine();
+                int userAnswerInt = new int();
+                if (!int.TryParse(userAnswerStr, out userAnswerInt)) continue;
+                switch (userAnswerInt) {
+                    case 0:
+                        exit = true;
+                        break;
+                    case 1:
+                        Add();
+                        break;
+                    case 2:
+                        Delete();
+                        break;
+                    case 3:
+                        Print();
+                        break;
+                    case 4:
+                        Sort();
+                        break;
+                    default:
+                        Console.WriteLine("Wrong number\n");
+                        break;
+                }
+            }
+        }
+
+        static void Add()
+        {
+            Console.WriteLine("Введите число, которое хотите добавить");
+            String userAnswer = Console.ReadLine();
+            Console.WriteLine(presentor.Add(userAnswer));
+        }
+
+        static void Delete()
+        {
+            Console.WriteLine("Введите индекс числа, которое хотите удалить");
+            String userAnswer = Console.ReadLine();
+            Console.WriteLine(presentor.Delete(userAnswer));
+        }
+        
+        static void Print()
+        {
+            Console.WriteLine(presentor.GetArrayString());
+        }
+        static void Sort()
+        {
+            presentor.SortArray();
+        }
+    }
+}
